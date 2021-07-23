2bc26163313f9428b20691e737b3b8f52f5902e7
dev-goutham <dev.gouthamram95@gmail.com>
2021-07-23 23:20:39 +0530

    build should fail

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { NextPage } from "next"
 
 const Index: NextPage = () => {
-	return <div>Hello World</div>
+	return <div>Hello Universe</div>
 }
 
 export default Index
